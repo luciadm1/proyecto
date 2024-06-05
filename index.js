18eb635e1bf9a033a97911ff2216a78f37a7ea9d
luciadm1 <luciademoraiz3@gmail.com>
2024-06-04 21:50:01 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var validarFormulario=document.getElementsByName("form")
+    getElementsByName=form
+    print(getElementsByName)
+
+var name=(){
+    if (form.nombre.value==0)
+      alert ("completar campo");
+
+}
+var validarTeléfono=(){
+    if (form.teléfono.value==0)
+    alert("Por favor verifica tu teléfono");
+}
+var validarMail=(){
+    if(form.mail.value==0)
+    alert("Porfavor,verifica tu mail")
+}
+var validarPassword=(){
+    if(form.name.password.value==0)
+    alert("Por favor, ingresa tu contraseña")
+}
